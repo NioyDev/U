ca2f4082c5a6d694b4805ae327053bac514e0dcc
5677 <166043734+NIOY-5677@users.noreply.github.com>
2026-04-21 00:21:41 -0300

    Html creados

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,32 @@
+let Opciones = {
+    "Inicio": "../index.html",
+    "Historia": "../html/historia.html",
+    "Requisitos": "../html/requesitos.html",
+    "Banda": "../html/banda.html",
+    "Descargas": "#",
+    "Contacto": "#"
+};
+
 let menu = document.getElementById("menu");
+let Desplegable = document.getElementById("MenuDesplegable")
+
 menu.innerHTML = `
-    <picture>
-            <img src="../assets/logo.png" alt="Logo">
-        </picture>
+    <picture><img src="../assets/logo.png" alt="Logo"></picture>
     <ul>
-        <li><a href="../index.html">Inicio</a></li>
-        <li><a href="../html/historia.html">Historia</a></li>
-        <li><a href="../html/requesitos.html">Requisitos</a></li>
-        <li><a href="#">Descargas</a></li>
-        <li><a href="#">Contacto</a></li>
+        ${Object.entries(Opciones).map(([nombre, link]) => `<li><a href="${link}">${nombre}</a></li>`).join('')}
     </ul>
 `;
+
+Desplegable.innerHTML = `
+        ${Object.entries(Opciones).map(([nombre, link]) => `<li><a href="${link}">${nombre}</a></li>`).join('')}
+`;
+
+
+const lenis = new Lenis();
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+  requestAnimationFrame(raf);
+
+
